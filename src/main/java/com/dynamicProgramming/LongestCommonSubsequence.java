8523caad6f8e5c87044eeea618a0d2367ab3b711
Kshitiz Gupta <kshitiz.gupta@izettle.com>
2021-03-04 20:38:05 +0100

    LongestCommonSubsequence in 2 strings

---
+++ src/main/java/com/dynamicProgramming/LongestCommonSubsequence.java
@@ -0,0 +1,56 @@
+package com.dynamicProgramming;
+
+/**
+ * Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+ *
+ * A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+ *
+ * For example, "ace" is a subsequence of "abcde".
+ * A common subsequence of two strings is a subsequence that is common to both strings.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: text1 = "abcde", text2 = "ace"
+ * Output: 3
+ * Explanation: The longest common subsequence is "ace" and its length is 3.
+ * Example 2:
+ *
+ * Input: text1 = "abc", text2 = "abc"
+ * Output: 3
+ * Explanation: The longest common subsequence is "abc" and its length is 3.
+ * Example 3:
+ *
+ * Input: text1 = "abc", text2 = "def"
+ * Output: 0
+ * Explanation: There is no such common subsequence, so the result is 0.
+ */
+public class LongestCommonSubsequence {
+    public static int solution(String text1, String text2) {
+        int[][] mat = new int[text1.length()+1][text2.length()+1];
+        for(int i=0; i<= text2.length(); i++) {
+            mat[0][i] = 0;
+        }
+
+        for(int i=0; i<= text1.length(); i++) {
+            mat[i][0] = 0;
+        }
+
+        for(int i=1; i<= text1.length(); i++) {
+            for(int j=1; j<= text2.length(); j++) {
+                if(text1.charAt(i-1) == text2.charAt(j-1)) {
+                    mat[i][j] = 1 + mat[i-1][j-1];
+                } else {
+                    mat[i][j] = Math.max(mat[i-1][j], mat[i][j-1]);
+                }
+            }
+        }
+
+        return mat[text1.length()][text2.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution("abcde", "abc"));
+    }
+}
